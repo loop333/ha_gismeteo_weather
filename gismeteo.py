d7396838057e77cbb39af898820f112c56b7bb96
Dmitry <37012903+loop333@users.noreply.github.com>
2019-02-22 05:08:39 +0500

    Update gismeteo.py

--- gismeteo.py
+++ gismeteo.py
@@ -4,13 +4,13 @@ Support for displaying weather info from Gismeteo API.
 import logging
 import voluptuous as vol
 
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 import urllib.request
 import xml.etree.ElementTree as ET
 
 from homeassistant.components.weather import (
-    ATTR_FORECAST_CONDITION, ATTR_FORECAST_TEMP, ATTR_FORECAST_TEMP_LOW,
-    ATTR_FORECAST_TIME, ATTR_FORECAST_WIND_SPEED, PLATFORM_SCHEMA, WeatherEntity)
+    ATTR_FORECAST_CONDITION, ATTR_FORECAST_TEMP, ATTR_FORECAST_TEMP_LOW, ATTR_FORECAST_PRECIPITATION,
+    ATTR_FORECAST_TIME, ATTR_FORECAST_WIND_SPEED, ATTR_FORECAST_WIND_BEARING, PLATFORM_SCHEMA, WeatherEntity)
 from homeassistant.const import (
     TEMP_CELSIUS, CONF_NAME, CONF_MODE)
 from homeassistant.helpers import config_validation as cv
@@ -41,6 +41,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_CITY): cv.string,
 })
 
+def dt_to_utc(dt, offset):
+    local_tz = timezone(timedelta(minutes=offset))
+    local_dt = datetime.fromisoformat(dt)
+    local_dt_with_tz = local_dt.replace(tzinfo=local_tz)
+    utc_dt = local_dt_with_tz.astimezone(timezone.utc)
+    return utc_dt.isoformat()
+
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Gismeteo weather"""
 
@@ -57,7 +64,7 @@ class GismeteoWeather(WeatherEntity):
 
     def __init__(self, name, city, mode):
         """Initialize the Gismeteo weather platform."""
-        _LOGGER.debug('gismeteo: _init_: %s %s %s', name, city, mode)
+#        _LOGGER.debug('gismeteo: _init_: %s %s %s', name, city, mode)
         self._name = name
         self._city = city
         self._mode = mode
@@ -128,12 +135,13 @@ class GismeteoWeather(WeatherEntity):
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
         """Get the latest state of the sensor."""
-        _LOGGER.debug('gismeteo: update')
+#        _LOGGER.debug('gismeteo: update')
 
         url = URL.format(self._city)
         resp = urllib.request.urlopen(url)
         xml = ET.parse(resp)
 
+        offset = int(xml.find('location').get('tzone'))
         fact = xml.find('location/fact/values')
 
         self._temperature = float(fact.get('tflt'))
@@ -151,24 +159,42 @@ class GismeteoWeather(WeatherEntity):
         if self._mode == 'daily':
             for d in xml.findall('location/day[@descr]'):
                 data_out = {}
-                data_out[ATTR_FORECAST_TIME] = d.get('date')
+                data_out[ATTR_FORECAST_TIME] = dt_to_utc(d.get('date'), offset)
                 data_out[ATTR_FORECAST_CONDITION] = d.get('descr')
                 data_out[ATTR_FORECAST_TEMP_LOW] = int(d.get('tmin'))
                 data_out[ATTR_FORECAST_TEMP] = int(d.get('tmax'))
+                data_out[ATTR_FORECAST_WIND_SPEED] = int(d.get('ws'))
+                wd = int(d.get('wd'))
+                if wd > 0: wd = wd - 1
+                data_out[ATTR_FORECAST_WIND_BEARING] = DIRECTIONS[wd]
+                pr = float(d.get('prflt', 0))
+                if pr == 0: pr = int(d.get('pr'))
+                data_out[ATTR_FORECAST_PRECIPITATION] = pr
                 self._forecast.append(data_out)
 
         if self._mode == 'hourly':
             for f in xml.findall('location/day/forecast'):
                 v = f.find('values')
+                ET.dump(v)
                 data_out = {}
-                data_out[ATTR_FORECAST_TIME] = f.get('valid')
+                data_out[ATTR_FORECAST_TIME] = dt_to_utc(f.get('valid'), offset)
                 data_out[ATTR_FORECAST_CONDITION] = v.get('descr')
 #                data_out[ATTR_FORECAST_TEMP_LOW] = int(d.get('tmin'))
                 data_out[ATTR_FORECAST_TEMP] = int(v.get('t'))
+                data_out[ATTR_FORECAST_WIND_SPEED] = int(v.get('ws'))
+                wd = int(v.get('wd'))
+                if wd > 0: wd = wd - 1
+                data_out[ATTR_FORECAST_WIND_BEARING] = DIRECTIONS[wd]
+                pr = float(v.get('prflt', 0))
+                if pr == 0: pr = int(v.get('pr'))
+                data_out[ATTR_FORECAST_PRECIPITATION] = pr
                 self._forecast.append(data_out)
 
 if __name__ == '__main__':
+#    print(dt_to_utc('2019-02-22T05:00:00', 300))
+#    quit()
     gismeteo = GismeteoWeather('gismeteo', '4517', 'hourly')
     gismeteo.update()
 #    print(gismeteo.wind_bearing)
-    print(gismeteo.forecast)
+    for i in gismeteo.forecast:
+        print(i)
